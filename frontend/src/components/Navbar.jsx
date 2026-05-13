486a98bd10e567134fe2d40e65b7d2ae2cd534ea
Tumhara Naam <tumhara@email.com>
2026-05-13 12:29:21 +0500

    hhb

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -81,7 +81,7 @@ export default function Navbar() {
       >
         <div className="max-w-7xl mx-auto md:px-4 px-0 nav-overflow-hidden">
           <nav
-            className={`flex items-center justify-between transition-all duration-300 ease-in-out rounded-none md:rounded-3xl px-4 py-1.5 md:px-6 md:py-3.5 border-x-0 border-t-0 md:border-x md:border-t border-b nav-overflow-hidden`}
+            className={`flex items-center justify-between transition-all duration-300 ease-in-out rounded-none md:rounded-3xl px-4  md:px-6 md:py-3.5 border-x-0 border-t-0 md:border-x md:border-t border-b nav-overflow-hidden`}
             style={{
               backgroundColor: scrolled ? 'var(--nav-bg-scrolled)' : 'var(--bg-deep)',
               borderColor: 'var(--nav-border)',
@@ -129,9 +129,9 @@ export default function Navbar() {
               {showBrandName && (
                 <span
                   className="font-display tracking-widest font-bold hidden sm:block truncate transition-colors duration-300 group-hover:text-(--gold)"
-                  style={{ 
+                  style={{
                     fontSize: navTitleSize,
-                    color: "var(--text-primary)" 
+                    color: "var(--text-primary)"
                   }}
                 >
                   {brandName.split(" ")[0]}
